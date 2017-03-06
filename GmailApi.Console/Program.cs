78a4189f66ea132ec1c515509b549548d7e0a832
Miguel Pelaez <mdpl11@gmail.com>
2017-03-05 20:37:50 -0500

    Incluir ejemplo del Api de Gogole Calendar.

--- GmailApi.Console/Program.cs
+++ GmailApi.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using GmailServiceInfraestructure;
+using Google.Apis.Calendar.v3.Data;
 using Google.Apis.Gmail.v1.Data;
 using System;
 using System.Collections.Generic;
@@ -9,13 +10,54 @@ namespace GmailQuickstart
     {
         private static void Main(string[] args)
         {
+            GmailSearch();
+            GmailSearchDetails();
+            GetUpcomingEvents();
+            Console.Read();
+        }
+
+        private static void GmailSearch()
+        {
             var gmailSearch = new GmailSearch
             {
                 Unread = true,
                 Label = new List<GmailSystemLabels>(new[] { GmailSystemLabels.Important })
             };
-            var messages = GmailApi.Instance.ListMessages(gmailSearch);
-            Console.Read();
+            List<Message> messages = GmailApi.Instance.ListMessages(gmailSearch);
+        }
+
+        private static void GmailSearchDetails()
+        {
+            var gmailSearch = new GmailSearch
+            {
+                Unread = true,
+                Label = new List<GmailSystemLabels>(new[] { GmailSystemLabels.Trash, GmailSystemLabels.Important }),
+            };
+
+            List<Message> messages = GmailApi.Instance.ListMessagesDetails(gmailSearch);
+        }
+
+        private static void GetUpcomingEvents()
+        {
+            Events events = CalendarApi.Instance.GetEvents();
+
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No upcoming events found.");
+            }
         }
     }
 }
---
+++ GmailServiceInfraestructure/CalendarApi.cs
@@ -0,0 +1,92 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GmailServiceInfraestructure
+{
+    public sealed class CalendarApi
+    {
+        private static volatile CalendarApi instance;
+        private static object syncRoot = new object();
+
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/calendar-dotnet-quickstart.json
+        private static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+
+        private static string ApplicationName = "Google Calendar API .NET";
+
+        private CalendarService service;
+
+        private CalendarApi()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        /// <summary>
+        /// Get an instance of the gmail api service.
+        /// </summary>
+        public static CalendarApi Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new CalendarApi();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public Events GetEvents()
+        {
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+
+            return events;
+        }
+    }
+}
